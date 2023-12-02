8b824e5bfa316cac6cfcfcf93445cae1ccd20304
Kaiser-Yang <624626089@qq.com>
2023-12-02 17:06:00 +0800

    Finish Luogu1028 DP

---
+++ Luogu/Luogu1028.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+const int MAXN = 1010;
+
+using namespace std;
+
+int n;
+int dp[MAXN];
+
+int main()
+{
+	cin >> n;
+	dp[0] = 1;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j <= i / 2; j++) {
+			dp[i] += dp[j];
+		}
+	}
+	cout << dp[n] << endl;
+	return 0;
+}
