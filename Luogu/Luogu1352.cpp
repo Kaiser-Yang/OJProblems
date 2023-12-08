2e4b5b89effed46c750fe4627472143bb3959cc0
Kaiser-Yang <624626089@qq.com>
2023-12-08 20:51:35 +0800

    Finish Luogu1352.cpp DP on a Tree

---
+++ Luogu/Luogu1352.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int MAXN = 6e3 + 10;
+
+int n, root;
+int happiness[MAXN], parent[MAXN], dp[MAXN][2];
+vector<int> g[MAXN];
+
+void addEdge(int u, int v)
+{
+	g[u].emplace_back(v);
+}
+
+void dfs(int u)
+{
+	int sum1 = 0, sum2 = 0;
+	for (int i = 0; i < g[u].size(); i++) {
+		int v = g[u][i];
+		dfs(v);
+		sum1 += max(dp[v][0], dp[v][1]);
+		sum2 += dp[v][0];
+	}
+	dp[u][0] = sum1;
+	dp[u][1] = sum2 + happiness[u];
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin >> n;
+	for (int i = 1; i <= n; i++) { cin >> happiness[i]; }
+	for (int i = 1; i < n; i++) {
+		int k, l;
+		cin >> l >> k;
+		parent[l] = k;
+		addEdge(k, l);
+
+	}
+	for (int i = 1; i <= n; i++) {
+		if (parent[i] == 0) {
+			root = i;
+			break;
+		}
+	}
+	dfs(root);
+	cout << max(dp[root][0], dp[root][1]) << endl;
+	return 0;
+}
