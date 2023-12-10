613af59fb8f4823147eccf17966961fafffd93bb
Kaiser-Yang <624626089@qq.com>
2023-12-10 15:33:39 +0800

    Modify filename: Luogu1401.cpp ---> Luogu1403.cpp

---
+++ Luogu/Luogu1403.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+long long ans;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin >> n;
+	for (int i = 1; i <= n; i++) { ans += n / i; }
+	cout << ans << endl;
+	return 0;
+}
