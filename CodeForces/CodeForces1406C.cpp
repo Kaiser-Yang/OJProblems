52b4f08bcb4b06f9062d39493da705133904280d
Kaiser-Yang <624626089@qq.com>
2024-01-12 14:45:55 +0800

    Finish CodeForces1406C.cpp Tree's Centroid

---
+++ CodeForces/CodeForces1406C.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int MAXN = 1e5 + 10;
+
+int T, n, u, v, fathest, fathestParent;
+int size[MAXN], depth[MAXN];
+
+vector<int> g[MAXN], centroid;
+
+void dfs(int u, int par)
+{
+    size[u] = 1;
+    int maxSonSize = 0;
+    for (int v : g[u]) {
+        if (v == par) { continue; }
+        dfs(v, u);
+        size[u] += size[v];
+        maxSonSize = max(maxSonSize, size[v]);
+    }
+    if (maxSonSize <= n / 2 && n - size[u] <= n / 2) {
+        centroid.push_back(u);
+    }
+}
+
+void fathestNode(int u, int par)
+{
+    depth[u] = depth[par] + 1;
+    if (depth[u] > depth[fathest]) {
+        fathest = u;
+        fathestParent = par;
+    }
+    for (int v : g[u]) {
+        if (v == par || v == centroid[1]) { continue; }
+        fathestNode(v, u);
+    }
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+    cin >> T;
+    while (T--) {
+        cin >> n;
+        for_each(g, g + 1 + n, [] (auto &v) { v.clear(); });
+        centroid.clear();
+        for (int i = 1; i < n; i++) {
+            cin >> u >> v;
+            g[u].push_back(v);
+            g[v].push_back(u);
+        }
+        dfs(1, 0);
+        if (centroid.size() == 1) {
+            cout << u << " " << v << endl;
+            cout << u << " " << v << endl;
+            continue;
+        }
+        depth[0] = -1;
+        fathest = fathestParent = 0;
+        fathestNode(centroid[0], 0);
+        cout << fathest << " " << fathestParent << endl;
+        cout << fathest << " " << centroid[1] << endl;
+    }
+	return 0;
+}
