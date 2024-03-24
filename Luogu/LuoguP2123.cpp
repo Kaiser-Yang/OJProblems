1ce6d9e9e80365dcef3d6b8d236253853ebe68e0
Kaiser-Yang <624626089@qq.com>
2024-03-24 11:44:19 +0800

    Finish LuoguP2123.cpp Greedy

---
+++ Luogu/LuoguP2123.cpp
@@ -0,0 +1,32 @@
+// problem statement: https://www.luogu.com.cn/problem/P2123
+
+#include <bits/stdc++.h>
+
+constexpr int MAXN = 2e4 + 10;
+
+using namespace std;
+
+int n, T;
+pair<long long, long long> minister[MAXN];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+    cin >> T;
+    while (T--) {
+        cin >> n;
+        for (int i = 0; i < n; i++) { cin >> minister[i].first >> minister[i].second; }
+        sort(minister, minister + n, [] (const auto &a, const auto &b) {
+            return min(a.first, b.second) == min(b.first, a.second) ?
+                   a.first < b.first :
+                   min(a.first, b.second) < min(b.first, a.second);
+        });
+        long long ans = 0, sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += minister[i].first;
+            ans = max(ans, sum) + minister[i].second;
+        }
+        cout << ans << "\n";
+    }
+	return 0;
+}
