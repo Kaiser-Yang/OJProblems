9cd4069e0092fba28fb9b703098bfc05a7eefd32
Kaiser-Yang <624626089@qq.com>
2024-03-25 17:23:03 +0800

    Finish LeetCode179.cpp

---
+++ LeetCode/LeetCode179.cpp
@@ -0,0 +1,16 @@
+// problem statement: https://leetcode.cn/problems/largest-number/
+
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), [] (const auto &a, const auto &b) {
+            return to_string(a) + to_string(b) > to_string(b) + to_string(a);
+        });
+        string ans = "";
+        for (auto &&num : nums) { ans += to_string(num); }
+        reverse(ans.begin(), ans.end());
+        while (ans.size() > 1 && ans.back() == '0') { ans.pop_back(); }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
