78001260c70e939f03872cb65f766f21b40c309e
Kaiser-Yang <624626089@qq.com>
2024-03-25 16:56:34 +0800

    Finish LeetCode122.cpp

---
+++ LeetCode/LeetCode122.cpp
@@ -0,0 +1,17 @@
+// problem statement: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-ii/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        int buyPrice = prices[0];
+        int i = 0;
+        while (i + 1 < prices.size()) {
+            while (i + 1 < prices.size() && prices[i + 1] >= prices[i]) { i++; }
+            ans += prices[i] - buyPrice;
+            while (i + 1 < prices.size() && prices[i + 1] <= prices[i]) { i++; }
+            buyPrice = prices[i];
+        }
+        return ans;
+    }
+};
