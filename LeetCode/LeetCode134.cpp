8c4d6e01c95463ef896f6518a4af0363db118874
Kaiser-Yang <624626089@qq.com>
2024-03-25 17:06:19 +0800

    Finish LeetCode134.cpp

---
+++ LeetCode/LeetCode134.cpp
@@ -0,0 +1,19 @@
+// problem statement: https://leetcode.cn/problems/gas-station/description/
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        vector<int> difference(n * 2);
+        int sum = 0;
+        for (int i = 0; i < n; i++) { difference[i] = difference[i + n] = gas[i] - cost[i]; }
+        int l = 0, r = 0;
+        while (l < n) {
+            while (sum + difference[r] >= 0 && l + n > r) { sum += difference[r++]; }
+            if (r - l == n) { return l; }
+            sum -= difference[l];
+            l++;
+        }
+        return -1;
+    }
+};
