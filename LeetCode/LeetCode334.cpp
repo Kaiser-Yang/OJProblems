c333a721dca7bd866513cc050a8f9c7f23696152
Kaiser-Yang <624626089@qq.com>
2024-03-30 11:13:39 +0800

    Finish LeetCode334.cpp

---
+++ LeetCode/LeetCode334.cpp
@@ -0,0 +1,18 @@
+// problem statement: https://leetcode.cn/problems/increasing-triplet-subsequence/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int minVal = nums[0], subMinVal = numeric_limits<int>::max();
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > subMinVal) { return true; }
+            else if (nums[i] > minVal) { subMinVal = nums[i]; }
+            else  { minVal = nums[i]; }
+        }
+        return false;
+    }
+};
