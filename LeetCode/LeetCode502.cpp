33361d6d6403614a9c1bd46cb1e9589b8053ab99
Kaiser-Yang <624626089@qq.com>
2024-04-02 17:19:30 +0800

    Finish LeetCode502.cpp

---
+++ LeetCode/LeetCode502.cpp
@@ -0,0 +1,37 @@
+// problem statement: https://leetcode.cn/problems/ipo/description/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n = profits.size();
+        vector<pair<int, int>> twoCombination(n);
+        for (int i = 0; i < n; i++) { twoCombination[i] = {profits[i], capital[i]}; }
+        sort(twoCombination.begin(), twoCombination.end(), [&capital] (const auto &a, const auto &b) {
+            return a.second < b.second;
+        });
+        priority_queue<int> q;
+        int i = 0;
+        while (i < n) {
+            if (twoCombination[i].second <= w) {
+                q.push(twoCombination[i].first);
+                i++;
+                continue;
+            }
+            if (!q.empty() && k > 0) {
+                k--;
+                w += q.top();
+                q.pop();
+            }
+            else { break; }
+        }
+        while (!q.empty() && k > 0) {
+            k--;
+            w += q.top();
+            q.pop();
+        }
+        return w;
+    }
+};
