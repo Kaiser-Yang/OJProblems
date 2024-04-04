e5114984e0316550fb989ceeb5fc9b1c6ff553ab
Kaiser-Yang <624626089@qq.com>
2024-04-04 10:11:01 +0800

    Finish LeetCode452.cpp

--- LeetCode/LeetCode435.cpp
+++ LeetCode/LeetCode435.cpp
@@ -3,6 +3,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
---
+++ LeetCode/LeetCode452.cpp
@@ -0,0 +1,22 @@
+// problem statement: https://leetcode.cn/problems/minimum-number-of-arrows-to-burst-balloons/description/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        auto &&cmp = [] (const auto &a, const auto &b) {
+            return a[1] < b[1];
+        };
+        sort(points.begin(), points.end(), cmp);
+        int ans = 1, nowRight = points[0][1];
+        for (int i = 1; i < points.size(); i++) {
+            if (points[i][0] <= nowRight) { continue; }
+            nowRight = points[i][1];
+            ans++;
+        }
+        return ans;
+    }
+};
