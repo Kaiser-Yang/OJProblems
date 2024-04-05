98b4bea20104b16d5e3418639fa0ff12d42c026f
Kaiser-Yang <624626089@qq.com>
2024-04-05 11:21:56 +0800

    Finish LeetCode659.cpp

---
+++ LeetCode/LeetCode659.cpp
@@ -0,0 +1,30 @@
+// problem statement: https://leetcode.cn/problems/split-array-into-consecutive-subsequences/description/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        unordered_map<int, int> cnt1;
+        unordered_map<int, int> cnt2;
+        for (int num : nums) { cnt1[num]++; }
+        for (int num : nums) {
+            while (cnt1[num] > 0) {
+                if (cnt2[num - 1] == 0) {
+                    if (cnt1[num + 1] == 0 || cnt1[num + 2] == 0) { return false; }
+                    cnt1[num]--;
+                    cnt1[num + 1]--;
+                    cnt1[num + 2]--;
+                    cnt2[num + 2]++;
+                } else {
+                    cnt1[num]--;
+                    cnt2[num - 1]--;
+                    cnt2[num]++;
+                }
+            }
+        }
+        return true;
+    }
+};
