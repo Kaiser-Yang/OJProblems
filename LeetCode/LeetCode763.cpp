54e2d6ce10a6827a0045ba3af4b01387b39c5e30
Kaiser-Yang <624626089@qq.com>
2024-04-06 19:04:18 +0800

    Finish LeetCode763.cpp

---
+++ LeetCode/LeetCode763.cpp
@@ -0,0 +1,24 @@
+// problem statement: https://leetcode.cn/problems/partition-labels/description/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        unordered_map<char, int> rightestPos;
+        vector<int> ans;
+        for (int i = 0; i < s.length(); i++) { rightestPos[s[i]] = i; }
+        int i = 0;
+        while (i < s.length()) {
+            int right = rightestPos[i];
+            for (int j = i; j <= right; j++) {
+                right = max(right, rightestPos[j]);
+            }
+            ans.push_back(right - i + 1);
+            i = right + 1;
+        }
+        return ans;
+    }
+};
