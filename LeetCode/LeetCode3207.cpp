dd71ee93a24b29a3f5c1947c80104ebb1da4e997
Kaiser-Yang <624626089@qq.com>
2024-09-06 09:42:08 +0800

    Finish LeetCode3207.cpp

---
+++ LeetCode/LeetCode3207.cpp
@@ -0,0 +1,17 @@
+// problem statement: https://leetcode.cn/problems/maximum-points-after-enemy-battles/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    long long maximumPoints(vector<int>& enemyEnergies, int currentEnergy) {
+        sort(enemyEnergies.begin(), enemyEnergies.end());
+        long long ans = 0;
+        long long sum = accumulate(enemyEnergies.begin(), enemyEnergies.end(), 0LL);
+        sum -= enemyEnergies[0];
+        if (currentEnergy >= enemyEnergies[0]) { ans += (sum + currentEnergy) / enemyEnergies[0]; }
+        return ans;
+    }
+};
