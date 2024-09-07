2c19ca52734e3f443b887dd602a5d69031f863b6
Kaiser-Yang <624626089@qq.com>
2024-09-07 10:19:38 +0800

    Finish LeetCode3202.cpp

---
+++ LeetCode/LeetCode3202.cpp
@@ -0,0 +1,20 @@
+// problem statement: https://leetcode.cn/problems/find-the-maximum-length-of-valid-subsequence-ii/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumLength(vector<int>& nums, int k) {
+        int n = nums.size(), ans = 0;
+        for (int rest = 0; rest < k; rest++) {
+            vector<int> dp(k);
+            for (int num : nums) {
+                dp[num % k] = dp[(rest - num % k + k) % k] + 1;
+                ans = max(ans, dp[num % k]);
+            }
+        }
+        return ans;
+    }
+};
