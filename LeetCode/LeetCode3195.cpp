8c0b24101bfc4892f09caf52c90a38504783db0c
Kaiser-Yang <624626089@qq.com>
2024-09-12 10:53:43 +0800

    Finish LeetCode3195.cpp

---
+++ LeetCode/LeetCode3195.cpp
@@ -0,0 +1,22 @@
+// problem statement: https://leetcode.cn/problems/find-the-minimum-area-to-cover-all-ones-i/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumArea(vector<vector<int>>& grid) {
+        int l = numeric_limits<int>::max(), r = numeric_limits<int>::min(), u = numeric_limits<int>::max(), d = numeric_limits<int>::min();
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] == 0) { continue; }
+                l = min(l, j);
+                r = max(r, j);
+                u = min(u, i);
+                d = max(d, i);
+            }
+        }
+        return (r - l + 1) * (d - u + 1);
+    }
+};
