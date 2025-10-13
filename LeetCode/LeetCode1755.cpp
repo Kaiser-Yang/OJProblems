56cf10b057b8c9139fc3bfa25ed09a70186a588c
Kaiser-Yang <58209855+Kaiser-Yang@users.noreply.github.com>
2025-10-13 14:28:52 +0000

    format(all): apply clang-format

--- LeetCode/LeetCode1755.cpp
+++ LeetCode/LeetCode1755.cpp
@@ -6,21 +6,15 @@ using namespace std;
 
 class Solution {
 public:
-    int minAbsDifference(vector<int>& nums, int goal) {
+    int minAbsDifference(vector<int> &nums, int goal) {
         vector<int> left, right;
-        for (int i = 0; i < nums.size() / 2; i++) {
-            left.push_back(nums[i]);
-        }
-        for (int i = nums.size() / 2; i < nums.size(); i++) {
-            right.push_back(nums[i]);
-        }
+        for (int i = 0; i < nums.size() / 2; i++) { left.push_back(nums[i]); }
+        for (int i = nums.size() / 2; i < nums.size(); i++) { right.push_back(nums[i]); }
         vector<int> sum;
         for (int s = 0; s < (1 << left.size()); s++) {
             int cur = 0;
             for (int i = 0; i < left.size(); i++) {
-                if (s & (1 << i)) {
-                    cur += left[i];
-                }
+                if (s & (1 << i)) { cur += left[i]; }
             }
             sum.push_back(cur);
         }
@@ -30,9 +24,7 @@ public:
         for (int s = 0; s < (1 << right.size()); s++) {
             int cur = 0;
             for (int i = 0; i < right.size(); i++) {
-                if (s & (1 << i)) {
-                    cur += right[i];
-                }
+                if (s & (1 << i)) { cur += right[i]; }
             }
             int target = goal - cur;
             auto lower = lower_bound(sum.begin(), sum.end(), target);
