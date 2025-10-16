438e9a7f993cdaeb488c0c2a2ec21ccbd0b09a32
Kaiser-Yang <624626089@qq.com>
2025-10-16 22:43:17 +0800

    chore: add Shoelace Formula

---
+++ template/computational_geometry.cpp
@@ -0,0 +1,16 @@
+#include <array>
+#include <vector>
+
+class computational_geometry {
+    public:
+        template<typename T, typename R = T>
+        static R polygon_area(const std::vector<std::array<T, 2>> &points) {
+            R area = 0;
+            for (int i = 0; i < points.size(); i++) {
+                int j = (i + 1) % points.size();
+                area += (R)points[i][0] * points[j][1];
+                area -= (R)points[i][1] * points[j][0];
+            }
+            return std::abs(area) / 2;
+        }
+};
