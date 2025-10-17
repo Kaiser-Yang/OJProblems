52931621b7bb9ba5ecb91e62b636cbd6ae1aa56a
Kaiser-Yang <624626089@qq.com>
2025-10-17 22:41:38 +0800

    chore: add manacher algorithm template

--- template/string_utils.cpp
+++ template/string_utils.cpp
@@ -48,6 +48,29 @@ class string_utils {
         return res;
     }
 
+    template <typename T>
+    std::vector<int> manacher(const T &s) {
+        int n = s.size();
+        if (n == 0) { return {}; }
+        std::vector<int> p(2 * n + 1);
+        T t(2 * n + 1, s[0]);
+        for (int i = 0; i < n; i++) {
+            t[2 * i] = s[0];
+            t[2 * i + 1] = s[i];
+        }
+        t[2 * n] = s[0];
+        int center = -1, right = 0;
+        for (int i = 0; i < t.size(); i++) {
+            if (i < right) { p[i] = std::min(p[2 * center - i], right - i); }
+            while (i - p[i] >= 0 && i + p[i] < t.size() && t[i - p[i]] == t[i + p[i]]) { p[i]++; }
+            if (i + p[i] > right) {
+                center = i;
+                right = i + p[i];
+            }
+        }
+        return p;
+    }
+
     template <int MAX_NODE_NUM, int ALPHABET_SIZE, typename id_generator>
     class aho_corasick_automaton {
     public:
