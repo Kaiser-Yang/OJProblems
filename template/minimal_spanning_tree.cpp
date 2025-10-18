159464fc5dea5a107e1c2186fb57279b9c6f2c38
Kaiser-Yang <624626089@qq.com>
2025-10-18 16:27:00 +0800

    chore: add a template for kruskal algorithm

--- template/minimal_spanning_tree.cpp
+++ template/minimal_spanning_tree.cpp
@@ -1,15 +1,15 @@
 #include <algorithm>
-#include <array>
 #include <cassert>
 #include <limits>
 #include <numeric>
 #include <queue>
 #include <utility>
 #include <vector>
+#include <tuple>
 
 class minimal_spanning_tree {
 public:
-    template <typename T, typename R>
+    template <typename T, typename R = int>
     static std::pair<std::vector<std::tuple<int, int, T>>, R> prim(
         const std::vector<std::vector<T>> &cost,
         R _ = int()) {
@@ -35,7 +35,7 @@ public:
             assert(u != -1);
             vis[u] = true;
             res += dist[u];
-            if (parent[u] != -1) { edges.push_back({u, parent[u], cost[u][parent[u]]}); }
+            if (parent[u] != -1) { edges.emplace_back(u, parent[u], cost[u][parent[u]]); }
             for (int v = 0; v < n; v++) {
                 if (!vis[v] && cost[u][v] < dist[v]) {
                     dist[v] = cost[u][v];
@@ -47,7 +47,7 @@ public:
         return {edges, res};
     }
 
-    template <typename T, typename R>
+    template <typename T, typename R = int>
     static std::pair<std::vector<std::tuple<int, int, T>>, R> prim(
         const std::vector<std::vector<std::pair<int, T>>> &g,
         R _ = int()) {
@@ -68,7 +68,7 @@ public:
             assert(u < n && par < n);
             q.pop();
             if (vis[u]) { continue; }
-            if (par != -1) { edges.push_back({u, par, d}); }
+            if (par != -1) { edges.emplace_back(u, par, d); }
             vis[u] = true;
             res += d;
             for (auto &&[v, w] : g[u]) {
@@ -82,5 +82,48 @@ public:
             return {{}, 0};  // graph is not connected
         }
         return {edges, res};
+    }
+
+    template <typename T, typename R = int>
+    static std::pair<std::vector<std::tuple<int, int, T>>, R> kruskal(
+        std::vector<std::tuple<int, int, T>> &edges,
+        R _ = int()) {
+        int n = 0;
+        for (int i = 0; i < edges.size(); i++) {
+            n = std::max(n, std::get<0>(edges[i]) + 1);
+            n = std::max(n, std::get<1>(edges[i]) + 1);
+        }
+        std::sort(edges.begin(), edges.end(), [](const auto &a, const auto &b) {
+            return std::get<2>(a) < std::get<2>(b);
+        });
+        std::vector<std::tuple<int, int, T>> mst_edges;
+        R res = 0;
+        int cnt = 0;
+        struct ufs {
+            std::vector<int> h;
+
+            ufs(int n) : h(n) { std::iota(h.begin(), h.end(), 0); }
+
+            int find(int u) { return h[u] == u ? u : h[u] = find(h[u]); }
+
+            bool same(int u, int v) { return find(u) == find(v); }
+
+            void unite(int u, int v) {
+                u = find(u);
+                v = find(v);
+                if (u == v) { return; }
+                h[v] = u;
+            }
+        } uf(n);
+        for (auto &&[u, v, w] : edges) {
+            if (uf.same(u, v)) { continue; }
+            uf.unite(u, v);
+            mst_edges.emplace_back(u, v, w);
+            res += w;
+            cnt++;
+            if (cnt == n - 1) { break; }
+        }
+        if (mst_edges.size() != n - 1) { return {{}, 0}; }
+        return {mst_edges, res};
     }
 };
