ac7f559a020340c2d0e2bc6cc3afa5d4bc0c83c6
Kaiser-Yang <624626089@qq.com>
2025-10-18 15:18:59 +0800

    chore

--- template/minimal_spanning_tree.cpp
+++ template/minimal_spanning_tree.cpp
@@ -1,19 +1,22 @@
 #include <array>
 #include <cassert>
 #include <limits>
+#include <queue>
 #include <utility>
 #include <vector>
 
 class minimal_spanning_tree {
 public:
-    template <typename T, typename R = T>
-    static std::pair<std::vector<std::array<int, 2>>, R> prim(
-        const std::vector<std::vector<T>> &cost) {
+    template <typename T, typename R>
+    static std::pair<std::vector<std::array<int, 3>>, R> prim(
+        const std::vector<std::vector<T>> &cost,
+        R _ = int()) {
         int n = cost.size();
+        assert(n > 0);
         std::vector<bool> vis(n);
         std::vector<R> dist(n, std::numeric_limits<R>::max());
         std::vector<int> parent(n, -1);
-        std::vector<std::array<int, 2>> edges;
+        std::vector<std::array<int, 3>> edges;
         dist[0] = 0;
         R res = 0;
         for (int i = 0; i < n; i++) {
@@ -24,7 +27,7 @@ public:
             assert(u != -1);
             vis[u] = true;
             res += dist[u];
-            if (parent[u] != -1) { edges.push_back({u, parent[u]}); }
+            if (parent[u] != -1) { edges.push_back({u, parent[u], cost[u][parent[u]]}); }
             for (int v = 0; v < n; v++) {
                 if (!vis[v] && cost[u][v] < dist[v]) {
                     dist[v] = cost[u][v];
@@ -32,6 +35,7 @@ public:
                 }
             }
         }
+        assert(edges.size() == n - 1);
         return {edges, res};
     }
 };
